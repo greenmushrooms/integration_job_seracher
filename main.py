31b1a7de5266a41fd027f921912aca9e724843f8
greenmushrooms <viacheslav.gorelik@gmail.com>
2025-11-23 21:52:52 -0500

    already loading in prefect.yaml

--- main.py
+++ main.py
@@ -5,21 +5,11 @@ import requests
 from jobspy import scrape_jobs
 from pandas import DataFrame
 from prefect import flow, runtime, task
-from prefect.blocks.system import Secret
 from prefect_dbt import PrefectDbtRunner, PrefectDbtSettings
 from sqlalchemy import create_engine, text
 
 from agent_eval import ClaudeJobEvaluator
 from helper import format_job_message_telegram, format_summary_message_telegram
-
-# Get database credentials
-DB_HOST = Secret.load("job-searcher--database-host", _sync=True).get()
-DB_PORT = Secret.load("job-searcher--database-port", _sync=True).get()
-DB_NAME = Secret.load("job-searcher--database-name", _sync=True).get()
-DB_USER = Secret.load("job-searcher--database-user", _sync=True).get()
-DB_PASSWORD = Secret.load("job-searcher--database-password", _sync=True).get()
-TELEGRAM_BOT_TOKEN = Secret.load("job-searcher--telegram-bot-token", _sync=True).get()
-TELEGRAM_CHAT_ID = Secret.load("job-searcher--telegram-chat-id", _sync=True).get()
 
 CONNECTION_STRING = (
     f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
